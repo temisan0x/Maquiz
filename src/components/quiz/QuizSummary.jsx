dc9e172a7757171eade59e3dd0eed762425903dd
Heytemisan <temyicy@gmail.com>
2021-12-06 22:50:08 +0100

    recreated UI

--- src/components/quiz/QuizSummary.jsx
+++ src/components/quiz/QuizSummary.jsx
@@ -1,6 +1,5 @@
 import React, {Component, Fragment} from 'react';
 import { Helmet } from 'react-helmet';
-import {Link} from 'react-router-dom';
 import "./QuizSummary.css"
 
 class QuizSummary extends Component {
